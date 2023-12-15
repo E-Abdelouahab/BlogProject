57da31e0d376fe690bd9a063631d3e0e4b6b1dc7
E-abdelouahab <abdelouahabelhassnaoui36@gmail.com>
2023-12-15 18:43:30 +0100

    delete use sweet alert

--- resources/views/admin/show_post.blade.php
+++ resources/views/admin/show_post.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head> 
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     @include('admin.css')
 
     <style type="text/css">
@@ -83,7 +86,7 @@
                     <img class="img_deg" src="postimage/{{$post->image}}">
                 </td>
                 <td>
-                    <a href="{{url('delete_post',$post->id)}}" type="button" class="btn btn-danger" onclick="return confirm('Are You sure to Delete This ? ')">Delete</a>
+                    <a href="{{url('delete_post',$post->id)}}" type="button" class="btn btn-danger" onclick="confirmation(event)">Delete</a>
                 </td>
                 
             </tr>	
@@ -95,5 +98,37 @@
 
 
       @include('admin.footer')
-  </body>
+  
+      <script type="text/javascript">
+
+      function confirmation(ev)
+      {
+        ev.preventDefault();
+        var urlToRedirect=ev.currentTarget.getAttribute('href');
+        
+        console.log(urlToRedirect);
+
+        swal({
+            title:"Are You sure to delete this?",
+            text: "You won't be able to revert this delete",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+
+        })
+
+        .then((willCancel)=>
+        {
+
+            if(willCancel)
+            {
+                window.location.href=urlToRedirect;
+            }
+
+        });
+      }
+    
+    
+    </script>
+    </body>
 </html>
